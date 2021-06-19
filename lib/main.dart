bc0c7c81f91bbb46e53ef05a00d42f85a6c3b01d
mohitvats2102 <mohitvatsktl@gmail.com>
2021-06-19 20:42:35 +0530

    Added Icon and splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:event_planner_buisness/screens/home_page.dart';
 import 'package:event_planner_buisness/screens/login_screen.dart';
 // ignore: import_of_legacy_library_into_null_safe
+import 'package:firebase_auth/firebase_auth.dart';
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -13,16 +15,24 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  late Widget _firstWidget;
+  final User _firebaseUser = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
+    // ignore: unnecessary_null_comparison
+    if (_firebaseUser == null)
+      _firstWidget = LoginScreen();
+    else
+      _firstWidget = HomePage();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         accentColor: Color(0xFFFF8038),
         primaryColor: Color(0xFF033249),
         fontFamily: 'Montserrat',
       ),
-      home: LoginScreen(),
+      home: _firstWidget,
       routes: {
         LoginScreen.loginScreen: (context) => LoginScreen(),
         HomePage.homePage: (context) => HomePage(),
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'login_screen.dart';
 
 class HomePage extends StatefulWidget {
   static const String homePage = '/home_page';
@@ -7,11 +11,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  void logout(BuildContext context) async {
+    if (_auth.currentUser.providerData != null) {
+      if (_auth.currentUser.providerData[0].providerId == 'google.com') {
+        print('In If BLOCK');
+        await GoogleSignIn().disconnect();
+      }
+    }
+    _auth.signOut();
+    Navigator.of(context).pushReplacementNamed(LoginScreen.loginScreen);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Event Planner'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                logout(context);
+              },
+              icon: Icon(Icons.logout))
+        ],
       ),
     );
   }
